0394cfb6ac6884582c615cc7eabe525eea49288e
lisaharv3y <lisaharv3y@hotmail.com>
2022-12-05 20:56:51 -0500

    search engine works

--- src/app.js
+++ src/app.js
@@ -39,8 +39,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-let apiKey = "f38a083dtf10b64a59b3baa446e088o9";
-let city = "Manila";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "f38a083dtf10b64a59b3baa446e088o9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Toronto");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
